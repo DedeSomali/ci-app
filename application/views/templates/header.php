e7d9cebc2078bf87dcf87dc4a4fc2f53623947d8
Dede Yusup Somali <onlinezimix@gmail.com>
2019-10-23 20:17:44 +0700

    Learning Tugas2

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,29 +1,33 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title><?= $data['judul'] ?></title>
-	<link rel="stylesheet" href="<?= BASEURL; ?>/css/bootstrap.css">
-</head>
-<body> 
+<!doctype html>
+<html lang="en">
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-	<div class="container">
-  <a class="navbar-brand" href="<?= BASEURL;?>">CI-App</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="<?= BASEURL;?>">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="<?= BASEURL;?>/mahasiswa">Mahasiswa</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="<?= BASEURL;?>/about">About</a>
-      </li>
-    </ul>
-  </div>
-</div>
-</nav> 
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?= base_url() ?>assets/css/bootstrap.css">
+
+    <title><?= $judul; ?></title>
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="<?= base_url('home/index'); ?>">CI App</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-item nav-link active" href="<?= base_url('home/index'); ?>">Home</a>
+                    <a class="nav-item nav-link active" href="<?= base_url('mahasiswa/index'); ?>">Mahasiswa</a>
+                     <a class="nav-item nav-link active" href="<?= base_url('peoples/index'); ?>">Peoples</a>
+                    <a class="nav-item nav-link" href="<?= base_url('about/index'); ?>">About</a>
+                </div>
+            </div>
+        </div>
+    </nav>
